867b2f0790802706998b3fe75c485def4a7845fa
Vasile Vlad <vladandreiy7@gmail.com>
2021-03-25 16:27:54 +0200

    removed JavaDoc

--- Auction_House/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.poo.proiect"},{"l":"com.poo.proiect.auctionhouse"},{"l":"com.poo.proiect.auctionhouse.simulation"},{"l":"com.poo.proiect.auctionhouse.utils"},{"l":"com.poo.proiect.client"},{"l":"com.poo.proiect.command"},{"l":"com.poo.proiect.product"}];updateSearchResults();
